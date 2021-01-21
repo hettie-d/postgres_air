35d0781bdcfdf4309c93afa9c492264a189f997c
hettie-d <henr33@gmail.com>
2021-01-21 11:24:05 -0600

    typo fix

--- functions/age_category.sql
+++ functions/age_category.sql
@@ -3,7 +3,7 @@ RETURNS TEXT language 'plpgsql' AS
 $body$
 BEGIN
     RETURN (case
-		WHEN p_age <= 25 then 'Infant'
+		WHEN p_age <= 2 then 'Infant'
 		WHEN p_age <=12 then 'Child'
 		WHEN p_age < 65 then 'Adult'
 		ELSE 'Senior' END);
